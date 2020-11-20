4e8b1d9cd96a47dd485868dfc11c89f62e8ecf1e
Sunil <svs57@cornell.edu>
2020-11-20 17:08:26 -0600

    push

---
+++ private.py
@@ -0,0 +1,4 @@
+consumer_key = "vBOCLiErTs0y9Yu3UKYQWiXfs"
+consumer_secret = "9B6q4tLvkEKKkUms2drqdUckhnG4MkgYjjVh3kGDijsR5zgyg6"
+access_token = "776426912914743296-3Zsy4hyD6hoMgagYcszJholK0iE55NP"
+access_token_secret = "TLwx4UMt7KWdHcTjJCCN9HFoOYX0K8AfEuMI1aNXFPIme"
